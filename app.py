0fe7c1d9373f00c37817278a6b9570c3ec32175b
Manpreet Singh <mampy.aman@gmail.com>
2024-03-20 14:03:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request
 import mysql.connector
 from flask_cors import CORS
 import json
-mysql = mysql.connector.connect(user='web', password='webPass',
-  host='127.0.0.1',
-  database='student')
 
+# MySQL connection
+mysql = mysql.connector.connect(
+    user='web',
+    password='webPass',
+    host='127.0.0.1',
+    database='student'
+)
+
+# Logging configuration
 from logging.config import dictConfig
-
 dictConfig({
     'version': 1,
     'formatters': {'default': {
@@ -25,11 +28,12 @@ dictConfig({
         'handlers': ['wsgi']
     }
 })
+
+# Flask app initialization
 app = Flask(__name__)
 CORS(app)
-# My SQL Instance configurations
-# Change the HOST IP and Password to match your instance configurations
 
+# Routes
 @app.route("/add", methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
@@ -41,29 +45,29 @@ def add():
         app.logger.info(s)
         cur.execute(s)
         mysql.commit()
-        return '{"Result":"Success"}' 
+        return '{"Result":"Success"}'
     else:
         return render_template('add.html')
 
-  return '{"Result":"Success"}'
-@app.route("/") #Default - Show Data
-def hello(): # Name of the method
-  cur = mysql.cursor() #create a connection to the SQL instance
-  cur.execute('''SELECT * FROM students''') # execute an SQL statment
-  rv = cur.fetchall() #Retreive all rows returend by the SQL statment
-  Results=[]
-  for row in rv: #Format the Output Results and add to return string
-    Result={}
-    Result['Name']=row[0].replace('\n',' ')
-    Result['Email']=row[1]
-    Result['ID']=row[2]
-    Results.append(Result)
-  response={'Results':Results, 'count':len(Results)}
-  ret=app.response_class(
-    response=json.dumps(response),
-    status=200,
-    mimetype='application/json'
-  )
-  return ret #Return the data in a string format
+@app.route("/")
+def hello():
+    cur = mysql.cursor()
+    cur.execute('''SELECT * FROM students''')
+    rv = cur.fetchall()
+    Results = []
+    for row in rv:
+        Result = {}
+        Result['Name'] = row[0].replace('\n', ' ')
+        Result['Email'] = row[1]
+        Result['ID'] = row[2]
+        Results.append(Result)
+    response = {'Results': Results, 'count': len(Results)}
+    ret = app.response_class(
+        response=json.dumps(response),
+        status=200,
+        mimetype='application/json'
+    )
+    return ret
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
+    app.run(host='0.0.0.0', port='8080')
