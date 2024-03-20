a7c28d3e18a055d1253ec7eeacacbd96989d5867
Manpreet Singh <mampy.aman@gmail.com>
2024-03-20 14:16:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask
+from flask import render_template
+from flask import request
 import mysql.connector
 from flask_cors import CORS
 import json
+mysql = mysql.connector.connect(user='web', password='webPass',
+  host='127.0.0.1',
+  database='student')
 
-# MySQL connection
-mysql = mysql.connector.connect(
-    user='web',
-    password='webPass',
-    host='127.0.0.1',
-    database='student'
-)
-
-# Logging configuration
 from logging.config import dictConfig
+
 dictConfig({
     'version': 1,
     'formatters': {'default': {
@@ -28,46 +25,52 @@ dictConfig({
         'handlers': ['wsgi']
     }
 })
-
-# Flask app initialization
 app = Flask(__name__)
 CORS(app)
+# My SQL Instance configurations
+# Change the HOST IP and Password to match your instance configurations
 
-# Routes
-@app.route("/add", methods=['GET', 'POST'])
+@app.route("/test")#URL leading to method
+def test(): # Name of the method
+ return("Hello World!<BR/>THIS IS ANOTHER TEST!") #indent this line
+
+@app.route("/yest")#URL leading to method
+def yest(): # Name of the method
+ return("Hello World!<BR/>THIS IS YET ANOTHER TEST!") #indent this line
+
+@app.route("/add", methods=['GET', 'POST']) #Add Student
 def add():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        print(name, email)
-        cur = mysql.cursor()
-        s = '''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name, email)
-        app.logger.info(s)
-        cur.execute(s)
-        mysql.commit()
-        return '{"Result":"Success"}'
-    else:
-        return render_template('add.html')
+  if request.method == 'POST':
+    name = request.form['name']
+    email = request.form['email']
+    print(name,email)
+    cur = mysql.cursor() #create a connection to the SQL instance
+    s='''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name,email)
+    app.logger.info(s)
+    cur.execute(s)
+    mysql.commit()
+  else:
+    return render_template('index.html')
 
-@app.route("/")
-def hello():
-    cur = mysql.cursor()
-    cur.execute('''SELECT * FROM students''')
-    rv = cur.fetchall()
-    Results = []
-    for row in rv:
-        Result = {}
-        Result['Name'] = row[0].replace('\n', ' ')
-        Result['Email'] = row[1]
-        Result['ID'] = row[2]
-        Results.append(Result)
-    response = {'Results': Results, 'count': len(Results)}
-    ret = app.response_class(
-        response=json.dumps(response),
-        status=200,
-        mimetype='application/json'
-    )
-    return ret
-
+  return '{"Result":"Success"}'
+@app.route("/") #Default - Show Data
+def hello(): # Name of the method
+  cur = mysql.cursor() #create a connection to the SQL instance
+  cur.execute('''SELECT * FROM students''') # execute an SQL statment
+  rv = cur.fetchall() #Retreive all rows returend by the SQL statment
+  Results=[]
+  for row in rv: #Format the Output Results and add to return string
+    Result={}
+    Result['Name']=row[0].replace('\n',' ')
+    Result['Email']=row[1]
+    Result['ID']=row[2]
+    Results.append(Result)
+  response={'Results':Results, 'count':len(Results)}
+  ret=app.response_class(
+    response=json.dumps(response),
+    status=200,
+    mimetype='application/json'
+  )
+  return ret #Return the data in a string format
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='8080')
+  app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
